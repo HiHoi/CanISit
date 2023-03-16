69313dff5c72fffb1e9b8386221f9bd65e699d33
donghyeon <ehdgud6634@gmail.com>
2023-03-16 18:48:57 +0900

    node_server refresh 3sec

--- node_server/app.js
+++ node_server/app.js
@@ -22,6 +22,26 @@ function checkVaildIp(subnetIp, req){ // npm i request-ip
     return(guestIp.startsWith(subnetIp));
 }
 
+setInterval(() => {
+    request('http://10.18.246.255:8000/', (error, response, body) => {
+      // 응답 처리
+        if (!error) {
+            const json = body;
+            const json_obj = JSON.parse(json);
+            var_data.data = json_obj.data;
+            var_data.timestamp = new Date().getTime();
+            var_data.image = json_obj.image;
+            var_data.resolution = json_obj.resolution;
+            now = new Date(var_data.timestamp);
+            var_data.time = now.toLocaleDateString('ko-KR') + " " + now.toLocaleTimeString('ko-KR');
+            for (let i = 0; i < var_data.data.length; i++) {
+                var_data.data[i].x = 650 * var_data.data[i].x / var_data.resolution.width;
+                var_data.data[i].y = 450 * var_data.data[i].y / var_data.resolution.height;
+            }
+        }
+    });
+}, 3000);
+
 app.set('port', process.env.PORT || 4000);
 
 var logger = function (req, res, next) {
@@ -35,7 +55,7 @@ app.get('/', (req, res) => {
     res.send('Hello, Express');
 });
 
-app.get('/data', (req, res) => {
+app.get('/test', (req, res) => {
     var_data.data = [{"x":0.0,"y":0.5,"type":"chair"},{"x":10.5,"y":5.8,"type":"table"},{"x":102,"y":30,"type":"sitting"},{"x":502,"y":430,"type":"chair"},{"x":351,"y":120,"type":"table"}, {"x":650,"y":450,"type":"table"},{"x":0,"y":0,"type":"table"},{"x":350,"y":350,"type":"table"}]
     var_data.timestamp = new Date().getTime();
     now = new Date(var_data.timestamp);
@@ -47,15 +67,15 @@ app.get('/data', (req, res) => {
     res.json(var_data);
 });
 
-app.get('/test', (req, res) => {
-    let ip = requestIp.getClientIp(req);
+app.get('/data', (req, res) => {
+    // let ip = requestIp.getClientIp(req);
     // 데이터 확인
     now = new Date().getTime();
-    if (now - var_data.timestamp > 5000 || var_data.timestamp == 0) {
+    if (var_data.timestamp == 0) {
         // 없거나  요청
-        const request = require('request');
+        // const request = require('request');
         const options = {
-            uri: "https://api.npoint.io/f4416f3f06c4a0b23a9c"
+            uri: "http://10.18.246.255:8000/"
         };
         function callback(error, response, body) {
             if (error || response.statusCode != 200) {
@@ -66,10 +86,52 @@ app.get('/test', (req, res) => {
                 const json = body;
                 const json_obj = JSON.parse(json);
                 var_data.data = json_obj.data;
-                var_data.timestamp = json_obj.timestamp;
+                var_data.timestamp = new Date().getTime();
                 var_data.image = json_obj.image;
+                var_data.resolution = json_obj.resolution;
                 now = new Date(var_data.timestamp);
                 var_data.time = now.toLocaleDateString('ko-KR') + " " + now.toLocaleTimeString('ko-KR');
+                for (let i = 0; i < var_data.data.length; i++) {
+                    var_data.data[i].x = 650 * var_data.data[i].x / var_data.resolution.width;
+                    var_data.data[i].y = 450 * var_data.data[i].y / var_data.resolution.height;
+                }
+                res.json(var_data);
+            }
+        }
+        request(options, callback);
+    }
+    else
+        res.json(var_data);
+});
+
+app.get('/datas', (req, res) => {
+    // let ip = requestIp.getClientIp(req);
+    now = new Date().getTime();
+    if (var_data.timestamp == 0) {
+        // 없거나  요청
+        // const request = require('request');
+        const options = {
+            uri: "http://10.18.246.255:8000/"
+        };
+        function callback(error, response, body) {
+            if (error || response.statusCode != 200) {
+                res.status(500).send(error.message);
+            }
+            else {
+                // console.log('Get new data');
+                const json = body;
+                const json_obj = JSON.parse(json);
+                var_data.data = json_obj.data;
+                var_data.timestamp = new Date().getTime();
+                var_data.image = json_obj.image;
+                var_data.resolution = json_obj.resolution;
+                now = new Date(var_data.timestamp);
+                var_data.time = now.toLocaleDateString('ko-KR') + " " + now.toLocaleTimeString('ko-KR');
+                for (let i = 0; i < var_data.data.length; i++) {
+                    var_data.data[i].x = 650 * var_data.data[i].x / var_data.resolution.width;
+                    var_data.data[i].y = 450 * var_data.data[i].y / var_data.resolution.height;
+                }
+                console.log(var_data);
                 res.json(var_data);
             }
         }
@@ -80,7 +142,7 @@ app.get('/test', (req, res) => {
     }
 });
 
-app.get('/image', (req, res) => {
+app.get('/cat', (req, res) => {
     var fs = require('fs');
     fs.readFile('./catcat.png', function (err, data) {
         if (err) throw err;
@@ -88,29 +150,12 @@ app.get('/image', (req, res) => {
         res.end(data);
         // res.send(data);
     });
-    // 데이터 확인
-    // now = new Date().getTime();
-    // if (now - var_data.timestamp > 5 || var_data.timestamp == 0) {
-    //     // 없으면 요청
-    //     const request = require('request');
-    //     const options = {
-    //         uri: "https://api.npoint.io/f4416f3f06c4a0b23a9c"
-    //     };
-    //     function callback(error, response, body) {
-    //         if (error || response.statusCode != 200) {
-    //             res.status(500).send(error.message);
-    //         }
-    //         else {
-    //             var_data = body;
-    //             console.log('Get new image');
-    //             res.send(var_data);
-    //         }
-    //     }
-    //     request(options, callback);
-    // }
-    // else {
-    //     res.send(var_data);
-    // }
+});
+
+app.get('/image', (req, res) => {
+    
+    res.redirect(var_data.image);
+    // res.send(var_data.image);
 });
 
 app.use((req, res) => {
