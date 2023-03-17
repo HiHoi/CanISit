314a37de03ac49251d70d8cf867c6a5f77c40510
donghyeon <ehdgud6634@gmail.com>
2023-03-17 17:16:52 +0900

    node_server complete

--- node_server/app copy.js
+++ node_server/app copy.js
@@ -1,5 +1,3 @@
-// npm start
-
 const express = require('express');
 const res = require('express/lib/response');
 const app = express();
@@ -12,9 +10,10 @@ app.use(cors({
     origin: '*',
 }));
 
-var var_data = new Object();
+var save_data = new Object();
+var new_data = new Object();
 
-function checkVaildIp(subnetIp, req){
+function checkVaildIp(subnetIp, req){ // npm i request-ip
     const requestIp = require('request-ip');
     let guestIp = requestIp.getClientIp(req);
     return(guestIp.startsWith(subnetIp));
@@ -26,19 +25,26 @@ function requestData() {
         url: "http://10.18.246.255:8000/",
         responseType: "json"
     })
-    .then(function(Response) {
-        var json_obj = Response.data;
+    .then(function(response) {
+        new_data = response.data;
+        if (!save_data)
+            save_data = new_data;
+        else {
+            
+        }
+
+
+        var json_obj = response.data;
         var_data.resolution = json_obj.resolution;
         var_data.data = json_obj.data;
-        var_data.image = json_obj.image;
         var_data.timestamp = new Date().getTime();
+        var_data.image = json_obj.image;
         now = new Date(var_data.timestamp);
         var_data.time = now.toLocaleDateString('ko-KR') + " " + now.toLocaleTimeString('ko-KR');
         for (let i = 0; i < var_data.data.length; i++) {
             var_data.data[i].x = 650 * var_data.data[i].x / var_data.resolution.width;
             var_data.data[i].y = 450 * var_data.data[i].y / var_data.resolution.height;
         }
-        console.log('request to muchine');
     })
     .catch(function (error) {
         console.log('\x1b[31m[Error_Server]\x1b[0m : ' + error.data);
@@ -64,8 +70,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/data', (req, res) => {
+    now = new Date().getTime();
     if (var_data.timestamp == 0) {
-        requestData();
+        setTimeout(() => requestData(), 1000);
     }
     res.json(var_data);
 });
@@ -84,3 +91,4 @@ app.listen(app.get('port'), '0.0.0.0' , () => {
     console.log(`\x1b[33m[LISTEN]\x1b[0m ${ip.address()}:${app.get('port')}`);
 });
 
+// npm start
